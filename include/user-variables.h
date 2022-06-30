058f2e0ff5c64b81457e70865cb31586f9f4aa22
System Administrator <root@MacBook-Air-de-Reymond.local>
2022-06-30 10:53:58 -0600

    first commit

---
+++ include/user-variables.h
@@ -0,0 +1,27 @@
+// *******************************************************************************************************************************
+// START userdefined data
+// *******************************************************************************************************************************
+#include <Arduino.h>
+
+//_______________________________CONEXIONES_____________________________________
+String dId = "9999";
+String webhook_pass = "nZnEJlxSDt";
+String webhook_endpoint = "https://YOUR_SERVER/api/getdevicecredentials";
+const char *server = "YOUR_SERVER";
+const int api_port = 3001;
+
+
+
+//WiFi
+
+const char *wifi_ssid = "SSID";
+const char *wifi_password = "WIFI_PASSWORD";
+
+//Interval for send to database
+const long sendBrokerInterval= 15000;
+const long sendDBInterval = 300000;
+
+
+// *******************************************************************************************************************************
+// END userdefined data
+// *******************************************************************************************************************************
